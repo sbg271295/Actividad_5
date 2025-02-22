ac3068451b07d534aa5a3cb4bcba6f6a6117b2d4
Sergio Bravo <s.bravoga1995@gmail.com>
2025-02-22 11:44:41 +0100

    Creacion de Componentes y interface

---
+++ blog_Noticias/src/app/components/form-notices/form-notices.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-notices',
+  imports: [],
+  templateUrl: './form-notices.component.html',
+  styleUrl: './form-notices.component.css'
+})
+export class FormNoticesComponent {
+
+}
---
+++ blog_Noticias/src/app/components/list-notices/list-notices.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-notices',
+  imports: [],
+  templateUrl: './list-notices.component.html',
+  styleUrl: './list-notices.component.css'
+})
+export class ListNoticesComponent {
+
+}
