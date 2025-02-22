45977a34af44359a91be3e19f856c3450f8fa56c
Sergio Bravo <s.bravoga1995@gmail.com>
2025-02-22 13:31:06 +0100

    Solucionado el formato de la fecha

--- blog_Noticias/src/app/components/list-notices/list-notices.component.ts
+++ blog_Noticias/src/app/components/list-notices/list-notices.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { INotice } from '../../interfaces/inotice.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-notices',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './list-notices.component.html',
   styleUrl: './list-notices.component.css'
 })
